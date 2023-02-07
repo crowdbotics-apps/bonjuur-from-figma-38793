2bbdd6840e03c18153f14c90de96244bd8513017
Crowdbotics <team@crowdbotics.com>
2023-02-07 17:30:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bonjuur-from-figma-38793",
+  url: "https://bonjuur-from-figma-38793.botics.co",
+  api: "https://bonjuur-from-figma-38793.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
